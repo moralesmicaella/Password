aff441b9777d94f5fe1a351def9503a5289dd805
Micaella Morales <morales.micaella@gmail.com>
2022-03-27 00:15:54 -0700

    feat: add layout for password view

---
+++ Password/Password/PasswordView.swift
@@ -0,0 +1,123 @@
+//
+//  PasswordView.swift
+//  Password
+//
+//  Created by Micaella Morales on 3/26/22.
+//
+
+import UIKit
+
+class PasswordView: UIView {
+    
+    let lockImageView = UIImageView(image: UIImage(systemName: "lock.fill"))
+    let passwordTextField = UITextField()
+    let eyeButton = UIButton(type: .custom)
+    let dividerView = UIView()
+    let errorMessageLabel = UILabel()
+    
+    var placeholder: String
+    
+    init(placeholder: String) {
+        self.placeholder = placeholder
+        
+        super.init(frame: .zero)
+        
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - STYLE AND LAYOUT
+extension PasswordView {
+    private func style() {
+        lockImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.keyboardType = .asciiCapable
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [.foregroundColor: UIColor.secondaryLabel])
+        passwordTextField.delegate = self
+        
+        eyeButton.translatesAutoresizingMaskIntoConstraints = false
+        eyeButton.setImage(UIImage(systemName: "eye.circle"), for: .normal)
+        eyeButton.setImage(UIImage(systemName: "eye.slash.circle"), for: .selected)
+        eyeButton.addTarget(self, action: #selector(togglePasswordView(_:)), for: .touchUpInside)
+        
+        
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.backgroundColor = .separator
+        
+        errorMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorMessageLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+        errorMessageLabel.textColor = .systemRed
+        errorMessageLabel.text = "Your password must meet the requirements below."
+        errorMessageLabel.numberOfLines = 0
+        errorMessageLabel.lineBreakMode = .byWordWrapping
+        errorMessageLabel.isHidden = false
+    }
+    
+    private func layout() {
+        addSubview(lockImageView)
+        addSubview(passwordTextField)
+        addSubview(eyeButton)
+        addSubview(dividerView)
+        addSubview(errorMessageLabel)
+        
+        // Lock Image
+        NSLayoutConstraint.activate([
+            lockImageView.topAnchor.constraint(equalTo: topAnchor),
+            lockImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+        ])
+        lockImageView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        
+        // Password Text Field
+        NSLayoutConstraint.activate([
+            passwordTextField.topAnchor.constraint(equalTo: topAnchor),
+            passwordTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: lockImageView.trailingAnchor, multiplier: 1),
+        ])
+        passwordTextField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+        // Eye Button
+        NSLayoutConstraint.activate([
+            eyeButton.topAnchor.constraint(equalTo: topAnchor),
+            eyeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: passwordTextField.trailingAnchor, multiplier: 1),
+            eyeButton.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        eyeButton.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        
+        // Divider
+        NSLayoutConstraint.activate([
+            dividerView.topAnchor.constraint(equalToSystemSpacingBelow: passwordTextField.bottomAnchor, multiplier: 1),
+            dividerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dividerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            dividerView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+        
+        // Error Label
+        NSLayoutConstraint.activate([
+            errorMessageLabel.topAnchor.constraint(equalTo: dividerView.bottomAnchor, constant: 4),
+            errorMessageLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            errorMessageLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            errorMessageLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - ACTIONS
+extension PasswordView {
+    @objc private func togglePasswordView(_ sender: UIButton) {
+        passwordTextField.isSecureTextEntry.toggle()
+        eyeButton.isSelected.toggle()
+    }
+}
+
+// MARK: - TEXT FIELD DELEGATE
+extension PasswordView: UITextFieldDelegate {
+    
+}
+
--- Password/Password/ViewController.swift
+++ Password/Password/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let newPasswordView = PasswordView(placeholder: "New Password")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
-
 
 }
 
+extension ViewController {
+    private func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        newPasswordView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(newPasswordView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2)
+        ])
+    }
+}
